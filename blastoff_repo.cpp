b1d7bb6118dfb896739b50be68b48156ba26407c
VirgillP <38685943+VirgillP@users.noreply.github.com>
2020-04-13 23:40:16 +0200

    Initial commit

---
+++ blastoff_repo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Let's", "try", "this", "again"};
+
+    int i = 0;
+    for (const string& word : msg)
+    {
+        ++i;
+
+        cout << word << " ";
+    }
+    cout << endl;
+}
